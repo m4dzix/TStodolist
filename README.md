ce0826e03d1f91e8144b3fece4c28ecb4c9d2e69
Magdalena Checinski <62214515+m4dzix@users.noreply.github.com>
2022-03-02 18:45:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# TO DO LIST
+
+![ToDoList](https://github.com/m4dzix/toDoList/blob/master/images/toDoListSmall.png?raw=true)
+
+Simple and basic to-do list - helps organise tasks.
+Made during the frontend developer course from [“youcode.pl”](https://youcode.pl).
+
+**Live link:** [https://m4dzix.github.io/todo-list/](https://m4dzix.github.io/todo-list/)
+
+## How to use:
+
+### Creating task
+
+If you want to add a new task to the list, you should:
+
+1. Enter the task content on the input area.
+
+2. Confirm by clicking button `Add task` or just press enter.
+
+Task content will show up as a part of the task list.
+
+### Managing task
+
+- Mark as complete: Click green box on left from task content.
+Second click on confirmed icon will reverse 'completed' animation.
+
+- Remove task: click red icon with `x`.
+
+- Mark all tasks as completed: click on button `MARK ALL TASKS AS COMPLETED` on menubar above task list.
+
+- Hide each completed task: click the `HIDE COMPLETED TASKS` button.
+When you click again, all completed tasks will be displayed.
+
+
+## Used technologies:
+
+- HTML
+- CSS
+- JavaScript(ES6)
+- BEM convention
+
+## Author:
+Magdalena Checinski - beginner | junior frontend developer
+
+## Contact :
+[madlen.checinski@gmail.com](mailto:madlen.checinski@gmail.com)
---
+++ script.js
@@ -0,0 +1,143 @@
+{
+  let tasks = [];
+  let hideDoneTasks = false;
+
+  const removeTask = (index) => {
+    tasks = [
+      ...tasks.slice(0, index), ...tasks.slice(index + 1)
+    ]
+    render();
+  };
+
+  const addNewTask = () => {
+    const newTaskContent = document.querySelector(".js-newTask").value.trim()
+    const markAllTasksCompltedButton = document.querySelector(".js-markAllDoneTasks")
+
+    if (newTaskContent !== "") {
+      tasks = [
+        ...tasks,
+        {
+          content: newTaskContent
+        },
+      ];
+      render();
+    };
+    document.querySelector(".js-newTask").value = ""
+  };
+
+  const toggleTaskDone = (doneButton, index) => {
+    tasks = [
+      ...tasks.slice(0, index),
+      {
+        ...tasks[index],
+        done: !tasks[index].done
+      },
+      ...tasks.slice(index + 1)
+    ];
+    render();
+  };
+
+  const toggleHideCompletedTasks = () => {
+    hideDoneTasks = !hideDoneTasks;
+    render();
+  };
+
+  const markAllTasksComplted = () => {
+    tasks = tasks.map((task) => ({
+      ...task,
+      done: true
+    }));
+    render();
+  };
+
+  bindTasksListener = () => {
+
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit);
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index)
+      });
+    });
+
+    const doneButtons = document.querySelectorAll(".js-done");
+
+    doneButtons.forEach((doneButton, index) => {
+      doneButton.addEventListener("click", () => {
+        toggleTaskDone(doneButton, index);
+      });
+    });
+
+  };
+  const bindTopButtonsListener = () => {
+
+    const toggleCompletedTasksButton = document.querySelector('.js-toggleHideAllDoneTasks')
+    toggleCompletedTasksButton.addEventListener("click", toggleHideCompletedTasks);
+
+    const markAllTasksCompltedButton = document.querySelector(".js-markAllDoneTasks");
+    markAllTasksCompltedButton.addEventListener("click", markAllTasksComplted);
+
+  };
+  const renderTaskContent = () => {
+    let htmlText = "";
+
+    for (const task of tasks) {
+      htmlText +=
+        `<li class="section__taskItem ${task.done && hideDoneTasks ? "section__taskItem--hidden" : ""}">
+        <button class="taskItem__button taskItem__button--doneTask js-done">
+    ${task.done ?'<i class="js-checkIcon fas fa-check"></i>': ''}
+        </button>
+        <p class="js-paragraph list__paragraph ${ task.done ? "list__paragraph--done" : "" }" > ${task.content}</p> 
+        <button class=" taskItem__button taskItem__button--removeTask js-remove">
+          <i class="fas fa-trash"></i></button>
+       </button>
+      </li>`
+    };
+
+    document.querySelector(".js-taskList").innerHTML = htmlText;
+  };
+
+  const renderButtons = () => {
+    let htmlButtonsText =
+      `
+      <button class="section__button js-toggleHideAllDoneTasks"${tasks.every(({
+        done}) => !done) ? "disabled":""}>
+        ${hideDoneTasks ? "show" : "hide"} completed tasks
+      </button>
+      <button class="section__button js-markAllDoneTasks" ${tasks.every(({
+      done}) => done) ? "disabled":""}>
+        Mark all tasks as completed
+      </button>
+      `;
+
+    const sectionButton = document.querySelector(".section__toggleButtons")
+    sectionButton.innerHTML = htmlButtonsText;
+
+    if (tasks.length) {
+      sectionButton.classList.remove("section__button--hidden")
+    } else {
+      sectionButton.classList.add("section__button--hidden")
+    };
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    addNewTask();
+    document.querySelector(".js-newTask").focus();
+  };
+  const render = () => {
+    renderButtons();
+    renderTaskContent();
+    bindTopButtonsListener();
+    bindTasksListener();
+  };
+
+  const init = () => {
+    render();
+  };
+
+  init();
+};
